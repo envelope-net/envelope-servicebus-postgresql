c2f85907c3ada9ec891655a34dd6820016dfad89
Tomas Pilat <tomas.pilat@disig.sk>
2022-10-31 09:05:57 +0100

    updated PostgreSqlJobRepository.SaveDataAsync

--- src/Envelope.ServiceBus.PostgreSql/Jobs/Internal/JobDataByNameQuery.cs
+++ src/Envelope.ServiceBus.PostgreSql/Jobs/Internal/JobDataByNameQuery.cs
@@ -4,11 +4,11 @@ using System.Linq.Expressions;
 
 namespace Envelope.ServiceBus.PostgreSql.Exchange.Internal;
 
-public class JobDataByNameQuery<TData> : ICompiledQuery<DbJobData<TData>, DbJobData<TData>?>
+public class JobDataByNameQuery : ICompiledQuery<DbJobData, DbJobData?>
 {
 	public string? JobName { get; set; }
 
-	public Expression<Func<IMartenQueryable<DbJobData<TData>>, DbJobData<TData>?>> QueryIs()
+	public Expression<Func<IMartenQueryable<DbJobData>, DbJobData?>> QueryIs()
 	{
 		return q => q.FirstOrDefault(x => x.JobName == JobName);
 	}

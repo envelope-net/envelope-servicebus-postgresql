792a265aa6fc4bf24b2e22e733c83f6ab282a3e3
Tomas Pilat <tomas.pilat@disig.sk>
2023-06-21 21:34:31 +0200

    updated IValidable implementation

--- src/Envelope.ServiceBus.PostgreSql/Configuration/PostgreSqlStoreConfiguration.cs
+++ src/Envelope.ServiceBus.PostgreSql/Configuration/PostgreSqlStoreConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿using Envelope.ServiceBus.PostgreSql.Internal;
-using Envelope.Text;
 using Envelope.Validation;
 
 namespace Envelope.ServiceBus.PostgreSql.Configuration;
@@ -17,24 +16,18 @@ public class PostgreSqlStoreConfiguration : IPostgreSqlStoreConfiguration, IVali
 
 #pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
-	public List<IValidationMessage>? Validate(string? propertyPrefix = null, List<IValidationMessage>? parentErrorBuffer = null, Dictionary<string, object>? validationContext = null)
+	public List<IValidationMessage>? Validate(
+		string? propertyPrefix = null,
+		ValidationBuilder? validationBuilder = null,
+		Dictionary<string, object>? globalValidationContext = null,
+		Dictionary<string, object>? customValidationContext = null)
 	{
-		if (StoreKey == default)
-		{
-			if (parentErrorBuffer == null)
-				parentErrorBuffer = new List<IValidationMessage>();
+		validationBuilder ??= new ValidationBuilder();
+		validationBuilder.SetValidationMessages(propertyPrefix, globalValidationContext)
+			.IfDefault(StoreKey)
+			.IfNullOrWhiteSpace(ConnectionString)
+			;
 
-			parentErrorBuffer.Add(ValidationMessageFactory.Error($"{StringHelper.ConcatIfNotNullOrEmpty(propertyPrefix, ".", nameof(StoreKey))} == null"));
-		}
-
-		if (string.IsNullOrWhiteSpace(ConnectionString))
-		{
-			if (parentErrorBuffer == null)
-				parentErrorBuffer = new List<IValidationMessage>();
-
-			parentErrorBuffer.Add(ValidationMessageFactory.Error($"{StringHelper.ConcatIfNotNullOrEmpty(propertyPrefix, ".", nameof(ConnectionString))} == null"));
-		}
-
-		return parentErrorBuffer;
+		return validationBuilder.Build();
 	}
 }
